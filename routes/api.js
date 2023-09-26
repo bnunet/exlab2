15d47bd4f84affe970c0093f2f7612936e47d85c
JiJi <i@mmdjiji.com>
2023-09-26 15:59:04 +0800

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -10,8 +10,4 @@ router.get('/echo', function(req, res, next) {
   res.send({ 'name': req.query.name });
 });
 
-router.get('/sum', (req, res) => {
-  res.send({ 'result': ~~req.query.a + ~~req.query.b})
-})
-
 module.exports = router;
